7c42f02b077361fa4a550ca908e43b2a39b82df9
ozazza <ozvacheva@gmail.com>
2023-09-27 01:04:21 +0300

    Guess a number - added difficulty steps, steps left, win and lose messages

--- guess_a_number.py
+++ guess_a_number.py
@@ -6,36 +6,41 @@ computer_number = random.randint(1, 100)
 steps_max = 6
 steps_counter = 0
 is_guessed = False
+while True:
+    while steps_counter < steps_max:
+        player_input = input('Guess the number (1-100): ')
 
-# Show difficulty
-print(f'')
-while steps_counter < steps_max:
-    player_input = input('Guess the number (1-100): ')
+        steps_counter += 1
+        steps_left = steps_max-steps_counter
 
-    steps_counter += 1
-    steps_left = steps_max-steps_counter
+        if not player_input.isdigit():
+            print('Invalid input. Try again...')
+            continue
+        else:
+            player_number = int(player_input)
 
-    if not player_input.isdigit():
-        print('Invalid input. Try again...')
-        continue
+            if player_number == computer_number:
+                print('You guess it!')
+                is_guessed = True
+                break
+            elif player_number < computer_number:
+                print('Too low!')
+            elif player_number > computer_number:
+                print('Too high!')
+
+        # Show difficulty
+        print(f'< Steps left {steps_left} >')
+
+    if is_guessed:
+        print('<<< Victory! You won the game! >>>')
     else:
-        player_number = int(player_input)
+        print(f'Sorry! The number was <<< {computer_number} >>>. Try another game!')
 
-        if player_number == computer_number:
-            print('You guess it!')
-            is_guessed = True
-            break
-        elif player_number < computer_number:
-            print('Too low!')
-        elif player_number > computer_number:
-            print('Too high!')
+    # Ask to play again
+    ask_player = input('Play again? (y / n): ')
+    if ask_player == 'y':
+        pass
+    else:
+        print('Okay, bye!')
+        break
 
-    # Show difficulty
-    print(f'< Steps left {steps_left} >')
-
-if is_guessed:
-    print('<<< Victory! You won the game! >>>')
-else:
-    print('Sorry! Try another game!')
-
-    print(f'steps: {steps_counter}')
