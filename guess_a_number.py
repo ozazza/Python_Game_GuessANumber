dda9e74a19933a5e7c7287912abdbda804b7ccd5
ozazza <ozvacheva@gmail.com>
2023-09-27 00:49:33 +0300

    Guess a number - added difficulty

--- guess_a_number.py
+++ guess_a_number.py
@@ -3,8 +3,17 @@ import random
 computer_number = random.randint(1, 100)
 
 # Read Player's Move
-while True:
+steps_max = 6
+steps_counter = 0
+is_guessed = False
+
+# Show difficulty
+print(f'')
+while steps_counter < steps_max:
     player_input = input('Guess the number (1-100): ')
+
+    steps_counter += 1
+    steps_left = steps_max-steps_counter
 
     if not player_input.isdigit():
         print('Invalid input. Try again...')
@@ -14,9 +23,19 @@ while True:
 
         if player_number == computer_number:
             print('You guess it!')
+            is_guessed = True
             break
         elif player_number < computer_number:
             print('Too low!')
-            continue
         elif player_number > computer_number:
             print('Too high!')
+
+    # Show difficulty
+    print(f'< Steps left {steps_left} >')
+
+if is_guessed:
+    print('<<< Victory! You won the game! >>>')
+else:
+    print('Sorry! Try another game!')
+
+    print(f'steps: {steps_counter}')
